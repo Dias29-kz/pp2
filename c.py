e3b14ea72eee75c3b03041a9cf500ec0b89714fa
Dias29-kz <d_igibai@kbtu.kz>
2026-01-30 11:28:45 +0500

    Practice 1 submission

---
+++ c.py
@@ -0,0 +1,15 @@
+text = "Hello world"
+print(text.upper())
+
+_text = "HELLO WORLD"
+print(_text.lower())
+
+v = "1234"
+print(v.isdigit())
+l = "12w34"
+print(v.isdigit())
+
+c = "dias"
+print(c.isalpha())
+x = "12dias"
+print(x.isalpha())
