3133bfe78d0daeb0a8090fc7b869177db375ce50
Dias29-kz <d_igibai@kbtu.kz>
2026-01-30 11:17:05 +0500

    Practice 1 submission

--- a.py
+++ a.py
@@ -2,5 +2,4 @@ print(4)
 print(5 * 8)
 print(10 - 6)
 print(2 + 2)
-
 print(20 // 5)
---
+++ z.py
@@ -0,0 +1,14 @@
+x = 2
+c = "Dias"
+print(x)
+print(c)
+
+#work in index
+
+s = "salem alem"
+print(s[0])
+#Here we was s because it is zero index
+
+b = "Hello World"
+print(b[-1])
+#Here that'll do d
