bb76a70c46ed62eedbe139558b5a4bac20e6e6fd
Dias29-kz <d_igibai@kbtu.kz>
2026-02-26 22:41:24 +0500

    branch main

---
+++ Your-Repository/math_exercises.py
@@ -0,0 +1,19 @@
+import math #import math we add mathematic functions
+
+x = math.sqrt(64) #sqrt it's uproot
+
+print(x)
+#Here min we give minimum value of the set, max same but maximum value 
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+#abs it's math function which just return positive numbers(module)
+x = abs(-7.25)
+
+print(x)
+#pow return the power of a number 
+x = pow(4, 3)
+
+print(x)
